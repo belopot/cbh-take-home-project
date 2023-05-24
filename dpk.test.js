8668b4d333aca2c79669bc84143670bfb07346be
belopot <belopot@hotmail.com>
2023-05-24 12:14:29 -0700

    Write unit tests

--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,36 @@
-const { deterministicPartitionKey } = require("./dpk");
+const { deterministicPartitionKey } = require('./dpk');
+const crypto = require('crypto');
 
-describe("deterministicPartitionKey", () => {
-  it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
+describe('deterministicPartitionKey', () => {
+  test('should return "0" if event is not provided', () => {
+    const result = deterministicPartitionKey();
+    expect(result).toBe("0");
+  });
+
+  test('should return the event.partitionKey if it exists', () => {
+    const event = {
+      partitionKey: "example"
+    };
+    const result = deterministicPartitionKey(event);
+    expect(result).toBe(event.partitionKey);
+  });
+
+  test('should return the hashed value of the event as the partition key if event.partitionKey is not present', () => {
+    const event = {
+      data: "example"
+    };
+    const dataHash = crypto.createHash('sha3-512').update(JSON.stringify(event)).digest('hex');
+    const result = deterministicPartitionKey(event);
+    expect(result).toBe(dataHash);
+  });
+
+  test('should return the hashed value of the candidate if it exceeds the maximum length', () => {
+    const event = {
+      data: "example",
+      partitionKey: "a".repeat(300)
+    };
+    const candidateHash = crypto.createHash('sha3-512').update(event.partitionKey).digest('hex');
+    const result = deterministicPartitionKey(event);
+    expect(result).toBe(candidateHash);
   });
 });
